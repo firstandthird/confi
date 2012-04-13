9b9e4f7c3736d66d8080e7bd4e88391ecbc7ca38
Greg Allen <me@jga.me>
2012-04-13 16:14:02 -0700

    aug deep for nested config settings

--- tests/confi.test.js
+++ tests/confi.test.js
@@ -2,8 +2,10 @@ var confi = require('../');
 
 exports.testDefault = function(t) {
   confi.reset();
-  var config = confi.load();
+  var config = confi.load({ env: 'default' });
   t.equal(config.host, 'localhost');
+  t.equal(config.analytics.enabled, true);
+  t.equal(config.analytics.profile, 'ga-xxx');
   t.done();
 };
 
@@ -12,12 +14,16 @@ exports.testDev = function(t) {
   var config = confi.load();
   t.equal(config.host, 'localhost');
   t.equal(config.apikey, 'asdfasdf');
+  t.equal(config.analytics.enabled, false);
+  t.equal(config.analytics.profile, 'ga-xxx');
   t.done();
 };
 
 exports.testProd = function(t) {
   confi.reset();
   var config = confi.load({ env: "production"});
+  t.equal(config.analytics.enabled, true);
+  t.equal(config.analytics.profile, 'ga-xxx');
   t.equal(config.host, 'prod');
   t.done();
 };
