edbdafdbfa5802c06af84804acd1acf363c0422b
orthagonal <orthagonal1@gmail.com>
2016-11-06 09:22:42 -0600

    options.package can also be a path to a package.json (#29)
    * allows 'options.package' to be a relative path
    
    * add test for path that includes 'package.json'

--- test/test-package-support.js
+++ test/test-package-support.js
@@ -1,6 +1,7 @@
 'use strict';
 /*global describe, it*/
 const chai = require('chai');
+const path = require('path');
 const assert = chai.assert;
 const confi = require('../');
 
@@ -25,6 +26,17 @@ describe('confi package.json support', () => {
       env: 'dev'
     });
     assert.equal(config.apikey, 'asdfasdf');
+    done();
+  });
+  it('will use an alternative package.json when the "package" option is a relative path', (done) => {
+    const config = confi({
+      package: 'conf4'
+    });
+    assert.equal(config.goodThings.comeIn, 'small packages');
+    const config2 = confi({
+      package: path.join('conf4', 'package.json')
+    });
+    assert.equal(config2.goodThings.comeIn, 'small packages');
     done();
   });
 });
