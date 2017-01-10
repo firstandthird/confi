2d5c55993d04a591ddf9780b11cceb860a75b465
Chris <orthagonal1@gmail.com>
2017-01-09 19:40:25 -0600

    supports 'key' and 'path' for package

--- test/test-package-support.js
+++ test/test-package-support.js
@@ -39,4 +39,32 @@ describe('confi package.json support', () => {
     assert.equal(config2.goodThings.comeIn, 'small packages');
     done();
   });
+  it('will use "key" field from package.json when the "package" option is an object with that key', (done) => {
+    const config = confi({
+      package: {
+        key: "won'tUnlockThisDoor"
+      }
+    });
+    assert.equal(config.goodThings, false);
+    done();
+  });
+  it('will get package.json from a specific "path"  when indicated', (done) => {
+    const config = confi({
+      package: {
+        path: path.join(__dirname, 'package')
+      }
+    });
+    assert.equal(config.aProperty, true);
+    done();
+  });
+  it('will get package.json from a specific "path"  and property when indicated', (done) => {
+    const config = confi({
+      package: {
+        key: "won'tUnlockThisDoor",
+        path: path.join(__dirname, 'package')
+      }
+    });
+    assert.equal(config.redHouse, true);
+    done();
+  });
 });
