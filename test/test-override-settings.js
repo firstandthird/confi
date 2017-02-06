8c1012fecd8fe47c52e677eb6288b89818ee5d17
chris <orthagonal1@gmail.com>
2017-02-05 20:16:45 -0600

    move to tape tests

--- test/test-override-settings.js
+++ test/test-override-settings.js
@@ -1,121 +1,117 @@
 'use strict';
 /*global describe, it*/
-const chai = require('chai');
-const assert = chai.assert;
+const tape = require('tape');
 const confi = require('../');
 
-describe('confi', () => {
-  it('can open the default file ', (done) => {
-    const config = confi({ env: 'default' });
-    assert.equal(config.host, 'localhost');
-    assert.equal(config.analytics.enabled, true);
-    assert.equal(config.analytics.profile, 'ga-xxx');
-    assert.equal(config.env, 'default');
-    done();
-  });
-  it('can open multiple paths', (done) => {
-    const config = confi({ env: 'default', path: ['./conf', './conf2'] });
-    assert.equal(config.host, 'localhost');
-    assert.equal(config.env, 'default');
-    assert.equal(config.multiple, true);
-    done();
-  });
-  it('can open the dev env', (done) => {
-    process.env.testEnv = 'test';
-    const config = confi();
-    assert.equal(config.host, 'localhost');
-    assert.equal(config.apikey, 'asdfasdf');
-    assert.equal(config.analytics.enabled, false);
-    assert.equal(config.analytics.profile, 'ga-xxx');
-    assert.equal(config.isTest, true);
-    assert.equal(config.testHost, 'localhost/test/path');
-    assert.equal(config.ENV.testEnv, 'test');
-    assert.equal(config.testDefault, '123456');
-    assert.equal(config.testDefault2, 'localhost');
-    assert.equal(config.testDefault3, '123456');
-    assert.equal(config.env, 'dev');
-    done();
-  });
-  it('can open the production env', (done) => {
-    const config = confi({ env: 'production' });
-    assert.equal(config.analytics.enabled, true);
-    assert.equal(config.analytics.profile, 'ga-xxx');
-    assert.equal(config.host, 'prod');
-    assert.equal(config.env, 'production');
-    done();
-  });
-  it('can open yaml files', (done) => {
-    const config = confi({ env: 'default' });
-    assert.equal(config.yaml, true);
-    done();
-  });
-  it('can open json files', (done) => {
-    const config = confi({ env: 'default' });
-    assert.equal(config.json, true);
-    done();
-  });
-  it('opens files with the environment prefix (eg default-plugin.json, default-route.yaml))', (done) => {
-    const config = confi({ env: 'default' });
-    assert.equal(config.auth, true);
-    assert.equal(config.plugin, true);
-    assert.equal(config.blah, true);
-    done();
-  });
+tape('can open the default file ', (assert) => {
+  const config = confi({ env: 'default' });
+  assert.equal(config.host, 'localhost');
+  assert.equal(config.analytics.enabled, true);
+  assert.equal(config.analytics.profile, 'ga-xxx');
+  assert.equal(config.env, 'default');
+  assert.end();
+});
+tape('can open multiple paths', (assert) => {
+  const config = confi({ env: 'default', path: ['./conf', './conf2'] });
+  assert.equal(config.host, 'localhost');
+  assert.equal(config.env, 'default');
+  assert.equal(config.multiple, true);
+  assert.end();
+});
+tape('can open the dev env', (assert) => {
+  process.env.testEnv = 'test';
+  const config = confi();
+  assert.equal(config.host, 'localhost');
+  assert.equal(config.apikey, 'asdfasdf');
+  assert.equal(config.analytics.enabled, false);
+  assert.equal(config.analytics.profile, 'ga-xxx');
+  assert.equal(config.isTest, true);
+  assert.equal(config.testHost, 'localhost/test/path');
+  assert.equal(config.ENV.testEnv, 'test');
+  assert.equal(config.testDefault, '123456');
+  assert.equal(config.testDefault2, 'localhost');
+  assert.equal(config.testDefault3, '123456');
+  assert.equal(config.env, 'dev');
+  assert.end();
+});
+tape('can open the production env', (assert) => {
+  const config = confi({ env: 'production' });
+  assert.equal(config.analytics.enabled, true);
+  assert.equal(config.analytics.profile, 'ga-xxx');
+  assert.equal(config.host, 'prod');
+  assert.equal(config.env, 'production');
+  assert.end();
+});
+tape('can open yaml files', (assert) => {
+  const config = confi({ env: 'default' });
+  assert.equal(config.yaml, true);
+  assert.end();
+});
+tape('can open json files', (assert) => {
+  const config = confi({ env: 'default' });
+  assert.equal(config.json, true);
+  assert.end();
+});
+tape('opens files with the environment prefix (eg default-plugin.json, default-route.yaml))', (assert) => {
+  const config = confi({ env: 'default' });
+  assert.equal(config.auth, true);
+  assert.equal(config.plugin, true);
+  assert.equal(config.blah, true);
+  assert.end();
+});
 
-  it('pulls in user config on top of default and env', (done) => {
-    const config = confi({ env: 'dev', user: 'jga' });
-    assert.equal(config.apikey, 'jga-key');
-    done();
-  });
+tape('pulls in user config on top of default and env', (assert) => {
+  const config = confi({ env: 'dev', user: 'jga' });
+  assert.equal(config.apikey, 'jga-key');
+  assert.end();
+});
 
-  it('should allow additional context to be passed directly into confi', (done) => {
-    const config = confi({
-      context: {
-        customData: true
-      }
-    });
-    assert.equal(config.customData, true);
-    done();
-  });
-
-
-  it('should support helper functions', (done) => {
-    const config = confi({
-      env: 'context',
-      helpers: {
-        getRandomNumber: () => Math.random()
-      }
-    });
-    assert.equal(typeof config.context.random, 'number');
-    //make sure it doesn't get added to data
-    assert.equal(typeof config.getRandomNumber, 'undefined');
-    done();
-  });
-
-  //unsupported right now
-  it.skip('should support helper functions that return functions', (done) => {
-    const config = confi({
-      env: 'function-helper',
-      helpers: {
-        getFunction: (value) => {
-          return function() {
-            return value;
-          };
-        }
-      }
-    });
-    assert.equal(typeof config.context.fn, 'function');
-    assert.equal(config.context.fn(), 'some value');
-    done();
-  });
-  it('throws an error if any files fail to parse', (done) => {
-    try {
-      const config = confi({ env: 'default', path: ['./dysfunctional'] });
-      // if we ever reach this line then something didn't work:
-      assert.equal(config, false);
-    } catch (exc) {
-      assert.equal(exc.message, 'Unable to parse file default.yaml')
+tape('should allow additional context to be passed directly into confi', (assert) => {
+  const config = confi({
+    context: {
+      customData: true
     }
-    done();
   });
+  assert.equal(config.customData, true);
+  assert.end();
+});
+
+tape('should support helper functions', (assert) => {
+  const config = confi({
+    env: 'context',
+    helpers: {
+      getRandomNumber: () => Math.random()
+    }
+  });
+  assert.equal(typeof config.context.random, 'number');
+  //make sure it doesn't get added to data
+  assert.equal(typeof config.getRandomNumber, 'undefined');
+  assert.end();
+});
+
+//unsupported right now
+tape.skip('should support helper functions that return functions', (assert) => {
+  const config = confi({
+    env: 'function-helper',
+    helpers: {
+      getFunction: (value) => {
+        return function() {
+          return value;
+        };
+      }
+    }
+  });
+  assert.equal(typeof config.context.fn, 'function');
+  assert.equal(config.context.fn(), 'some value');
+  assert.end();
+});
+tape('throws an error if any files fail to parse', (assert) => {
+  try {
+    const config = confi({ env: 'default', path: ['./dysfunctional'] });
+    // if we ever reach this line then something didn't work:
+    assert.equal(config, false);
+  } catch (exc) {
+    assert.equal(exc.message, 'Unable to parse file default.yaml')
+  }
+  assert.end();
 });
--- test/test-package-support.js
+++ test/test-package-support.js
@@ -1,70 +1,67 @@
 'use strict';
 /*global describe, it*/
-const chai = require('chai');
 const path = require('path');
-const assert = chai.assert;
+const tape = require('tape');
 const confi = require('../');
 
-describe('confi package.json support', () => {
-  it('will use defaults from package.json when the "package" option is true', (done) => {
-    const config = confi({
-      package: true
-    });
-    assert.equal(config.goodThings, true);
-    done();
+tape('will use defaults from package.json when the "package" option is true', (assert) => {
+  const config = confi({
+    package: true
   });
-  it('keys/values parsed from package.json overwrite the corresponding keys/values in default conf', (done) => {
-    const config = confi({
-      package: true
-    });
-    assert.equal(config.host, 'colbert');
-    done();
+  assert.equal(config.goodThings, true);
+  assert.end();
+});
+tape('keys/values parsed from package.json overwrite the corresponding keys/values in default conf', (assert) => {
+  const config = confi({
+    package: true
   });
-  it('keys/values parsed from package.json are overwritten by the corresponding keys/values in env conf', (done) => {
-    const config = confi({
-      package: true,
-      env: 'dev'
-    });
-    assert.equal(config.apikey, 'asdfasdf');
-    done();
+  assert.equal(config.host, 'colbert');
+  assert.end();
+});
+tape('keys/values parsed from package.json are overwritten by the corresponding keys/values in env conf', (assert) => {
+  const config = confi({
+    package: true,
+    env: 'dev'
   });
-  it('will use an alternative package.json when the "package" option is a relative path', (done) => {
-    const config = confi({
-      package: 'conf4'
-    });
-    assert.equal(config.goodThings.comeIn, 'small packages');
-    const config2 = confi({
-      package: path.join('conf4', 'package.json')
-    });
-    assert.equal(config2.goodThings.comeIn, 'small packages');
-    done();
+  assert.equal(config.apikey, 'asdfasdf');
+  assert.end();
+});
+tape('will use an alternative package.json when the "package" option is a relative path', (assert) => {
+  const config = confi({
+    package: 'conf4'
   });
-  it('will use "key" field from package.json when the "package" option is an object with that key', (done) => {
-    const config = confi({
-      package: {
-        key: "won'tUnlockThisDoor"
-      }
-    });
-    assert.equal(config.goodThings, false);
-    done();
+  assert.equal(config.goodThings.comeIn, 'small packages');
+  const config2 = confi({
+    package: path.join('conf4', 'package.json')
   });
-  it('will get package.json from a specific "path"  when indicated', (done) => {
-    const config = confi({
-      package: {
-        path: path.join(__dirname, 'package')
-      }
-    });
-    assert.equal(config.aProperty, true);
-    done();
+  assert.equal(config2.goodThings.comeIn, 'small packages');
+  assert.end();
+});
+tape('will use "key" field from package.json when the "package" option is an object with that key', (assert) => {
+  const config = confi({
+    package: {
+      key: "won'tUnlockThisDoor"
+    }
   });
-  it('will get package.json from a specific "path"  and property when indicated', (done) => {
-    const config = confi({
-      package: {
-        key: "won'tUnlockThisDoor",
-        path: path.join(__dirname, 'package')
-      }
-    });
-    assert.equal(config.redHouse, true);
-    done();
+  assert.equal(config.goodThings, false);
+  assert.end();
+});
+tape('will get package.json from a specific "path"  when indicated', (assert) => {
+  const config = confi({
+    package: {
+      path: path.join(__dirname, 'package')
+    }
   });
+  assert.equal(config.aProperty, true);
+  assert.end();
+});
+tape('will get package.json from a specific "path"  and property when indicated', (assert) => {
+  const config = confi({
+    package: {
+      key: "won'tUnlockThisDoor",
+      path: path.join(__dirname, 'package')
+    }
+  });
+  assert.equal(config.redHouse, true);
+  assert.end();
 });
