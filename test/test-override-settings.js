421d40197bad656dddcb634101e248b63377af56
orthagonal <orthagonal1@gmail.com>
2017-03-19 22:46:51 -0500

    tape -> tap

--- test/test-override-settings.js
+++ test/test-override-settings.js
@@ -1,6 +1,7 @@
 'use strict';
 /*global describe, it*/
-const tape = require('tape');
+const tape = require('tap').test;
+tape.skip = () => {};
 const confi = require('../');
 
 tape('can open the default file ', (assert) => {
