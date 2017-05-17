671c46cc934b4233724350624da4795051773001
Chris <orthagonal1@gmail.com>
2017-05-17 10:24:56 -0500

    make async

--- test/test-file-support.js
+++ test/test-file-support.js
@@ -5,42 +5,48 @@ const path = require('path');
 const tape = require('tape');
 
 tape('can use a prefix with default env to selectively filter which files to load', (assert) => {
-  const config = confi({
-    path: [{ path: path.join('.', 'conf3'), prefix: 'useit' }]
+  confi({
+    path: [{ path: path.join(__dirname, 'conf3'), prefix: 'useit' }]
+  }, (err, config) => {
+    // should have loaded conf3/useit.yaml but not donotload-this.yaml:
+    assert.equal(err, null, 'does not error');
+    assert.equal(config.host, 'localhost');
+    assert.equal(config.donotloadthis, undefined);
+    // should not have loaded conf3/useit-prod since we are in default env:
+    assert.equal(config.prod, undefined);
+    assert.end();
   });
-  // should have loaded conf3/useit.yaml but not donotload-this.yaml:
-  assert.equal(config.host, 'localhost');
-  assert.equal(config.donotloadthis, undefined);
-  // should not have loaded conf3/useit-prod since we are in default env:
-  assert.equal(config.prod, undefined);
-  assert.end();
 });
 
 tape('can use a prefix with dev env to selectively filter which files to load', (assert) => {
-  const config = confi({
+  confi({
     env: 'dev',
-    path: [{ path: path.join('.', 'conf3'), prefix: 'useit' }]
+    path: [{ path: path.join(__dirname, 'conf3'), prefix: 'useit' }]
+  }, (err, config) => {
+    assert.equal(err, null, 'does not error');
+    // should have loaded conf3/useit.yaml (we also load this no matter the env) but not donotload-this.yaml:
+    assert.equal(config.host, 'localhost');
+    assert.equal(config.donotloadthis, undefined);
+    // should have not loaded conf3/useit-prod.yaml since we are using the dev env:
+    assert.equal(config.prod, undefined);
+    // should have loaded conf3/useit-dev.yaml since we are in the dev env:
+    assert.equal(config.dev, 1);
+    assert.end();
   });
-  // should have loaded conf3/useit.yaml (we also load this no matter the env) but not donotload-this.yaml:
-  assert.equal(config.host, 'localhost');
-  assert.equal(config.donotloadthis, undefined);
-  // should have not loaded conf3/useit-prod.yaml since we are using the dev env:
-  assert.equal(config.prod, undefined);
-  // should have loaded conf3/useit-dev.yaml since we are in the dev env:
-  assert.equal(config.dev, 1);
-  assert.end();
 });
 tape('can use a prefix with prod env to selectively filter which files to load', (assert) => {
-  const config = confi({
+  confi({
     env: 'prod',
-    path: [{ path: path.join('.', 'conf3'), prefix: 'useit' }]
+    path: [{ path: path.join(__dirname, 'conf3'), prefix: 'useit' }]
+  }, (err, config) => {
+    assert.equal(err, null, 'does not error');
+    // should have loaded conf3/useit.yaml (we also load this no matter the env) but not donotload-this.yaml:
+    assert.equal(config.host, 'localhost');
+    assert.equal(config.donotloadthis, undefined);
+    // should have loaded conf3/useit-prod.yaml since we are using the prod env:
+    assert.equal(config.prod, true);
+    // should not have loaded conf3/useit-dev.yaml since we are in the prod env:
+    assert.equal(config.dev, undefined);
+    assert.end();
   });
-  // should have loaded conf3/useit.yaml (we also load this no matter the env) but not donotload-this.yaml:
-  assert.equal(config.host, 'localhost');
-  assert.equal(config.donotloadthis, undefined);
-  // should have loaded conf3/useit-prod.yaml since we are using the prod env:
-  assert.equal(config.prod, true);
-  // should not have loaded conf3/useit-dev.yaml since we are in the prod env:
-  assert.equal(config.dev, undefined);
-  assert.end();
 });
