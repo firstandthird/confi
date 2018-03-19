919b0adeed30fd09c2da4b8962afe38fb66628b5
orthagonal <orthagonal1@gmail.com>
2018-03-19 11:42:36 -0500

    Async (#64)
    * first pass
    
    * tap-spec?
    
    * updates

--- test/test-url-support.js
+++ test/test-url-support.js
@@ -3,46 +3,37 @@
 const confi = require('../');
 const tap = require('tap');
 const tape = tap.test;
-const async = require('async');
 const Hapi = require('hapi');
-
-let server;
-tap.beforeEach((done) => {
-  server = new Hapi.Server();
-  server.connection({
-    host: 'localhost',
-    port: 8000
-  });
-  server.route({
-    method: 'GET',
-    path: '/confi2',
-    handler(request, reply) {
-      return reply({ url1: true });
-    }
-  });
-  server.route({
-    method: 'GET',
-    path: '/confi1',
-    handler(request, reply) {
-      return reply('url1: true').type('application/yaml');
-    }
-  });
-  server.start(done);
-});
-
-tap.afterEach((done) => {
-  server.stop(done);
-});
 
 
 tape('can fetch json config from a remote url', async(assert) => {
+  const server = new Hapi.Server({ port: 8000 });
+  server.route({
+    method: 'GET',
+    path: '/confi1',
+    handler(request, h) {
+      return h.response({ url1: true }).type('application/json');
+    }
+  });
+  await server.start();
   const result = await confi({ url: 'http://localhost:8000/confi1' });
   assert.equal(result.url1, true);
+  await server.stop();
   assert.end();
 });
 
 tape('can fetch yaml config from a remote url', async(assert) => {
+  const server = new Hapi.Server({ port: 8000 });
+  server.route({
+    method: 'GET',
+    path: '/confi1',
+    handler(request, h) {
+      return h.response('url1: true').type('application/yaml');
+    }
+  });
+  await server.start();
   const result = await confi({ url: 'http://localhost:8000/confi1' });
   assert.equal(result.url1, true);
+  await server.stop();
   assert.end();
 });
